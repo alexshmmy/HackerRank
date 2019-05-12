23548735025ec536f6aec585cb7ba6b2eecd217c
alexshmmy <anikou@kth.se>
2019-05-12 13:37:04 +0200

    Create 007.array_intro.cpp

---
+++ 007.array_intro.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int i, j;
+    int n;
+
+    cin >> n;
+
+    int arr[n];
+
+    for (i = 0; i < n; i++)
+        cin >> arr[i];
+    
+
+    for (j = n-1; j >= 0; j = j -1)
+        cout << arr[j] << ' ';
+    
+    return 0;
+}
