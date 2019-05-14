a091161a8b950ac6c62666688f069884e9f67f5d
alexshmmy <anikou@kth.se>
2019-05-14 10:14:51 +0200

    Update 001.hello_world.cpp
    extra commands added

--- 001.hello_world.cpp
+++ 001.hello_world.cpp
@@ -2,8 +2,11 @@
 #include <cstdio>
 using namespace std;
 
-int main() {
-    printf("Hello, World!");
+int main(void) {
+    int age = 31;
+    
+    printf("Hello, World! \n");
+    cout << "My age is " << age " years old" << endl;
     
     return 0;
 }
