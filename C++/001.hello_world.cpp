2732b38ac68ff218e96ea5718cc18e4212407542
alexshmmy <anikou@kth.se>
2019-05-15 18:51:33 +0200

    Create 001.hello_world.cpp

---
+++ C++/001.hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(void) {
+    int age = 31;
+    
+    printf("Hello, World! \n");
+    cout << "My age is " << age << " years old" << endl;
+    
+    return 0;
+}
