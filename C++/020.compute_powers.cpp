b70d1f5c9965bba0e6a6d1800c87ce4eb73fab28
alexshmmy <anikou@kth.se>
2019-05-23 20:10:45 +0200

    Update 020.compute_powers.cpp

--- C++/020.compute_powers.cpp
+++ C++/020.compute_powers.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 // a function that computes the power of an integer
-int power(int, int);
+double power(double, int);
 
 int main(void) {
-    int b, e;
+    double b; // b stands for the base
+    int e; // e stands for the exponent
 
     cout << "Enter a number: " << endl;
     cin >> b;
@@ -20,14 +21,13 @@ int main(void) {
     return 0;
 }
 
-int power(int base, int exponent) {
-    int result = base;
+double power(double base, int exponent) {
+    double result = base;
 
     if (base == 0) {
         cout << "Base 0 is not allowed!" << endl;
         return -1;
     }
-
 
     for (int i = 2; i <= exponent; i++) {
         result *= base;
